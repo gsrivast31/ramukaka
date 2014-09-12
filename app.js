9e03ff5edd5377c9cd3d4f712fd38fa364f11109
Gaurav Srivastava <gaurav.sri87@gmail.com>
2014-09-12 23:00:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var routes = require('./routes/routes.js');
+var express = require('express');
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    //app.use(express.favicon(__dirname + '/public/img/favicon.png'));
+    //app.use(express.cookieParser());
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.set('view options', {
+    layout: false
+});
+
+app.get('/', routes.getIndex);
+
+app.get('/services', routes.getServices);
+
+app.post('/subscribe', routes.subscribeUser);
+
+app.listen(app.get('port'), function () {
+    console.log("Server listening on port " + app.get('port'));
+});
---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+var mcapi = require('../node_modules/mailchimp-api/mailchimp');
+
+var mc = new mcapi.Mailchimp('53b703bd208115173012aba8271f64a3-us8');
+
+module.exports = {
+  getIndex: function(req, res) {
+    res.render('index');
+  },
+
+  getServices: function(req, res) {
+    res.render('service');
+  },
+  
+  subscribeUser: function(req, res) {
+    mc.lists.subscribe({
+	    id:'f9fd81fd82', 
+	    email:{email:req.body.email},
+	    merge_vars: {zipcode:req.body.zipcode}
+	  }, function(data) {
+   	  res.send(200);
+    }, function(error) {
+      res.send(500);
+    });
+  }
+};
